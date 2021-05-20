730e38aab98d2adf3779105266db66129dafdce8
chaudharyamardeep <chaudharyamardeep@gmail.com>
2021-05-20 02:11:03 -0500

    added remove command

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-sudo nohup java -jar /home/ec2-user/code-deploy.jar > app.log 2>&1 &
+#!/usr/bin/env bash
+cd /home/ec2-user
+sudo /usr/bin/java -jar -Dserver.port=80 \
+    *.jar > /dev/null 2> /dev/null < /dev/null &
+#sudo nohup java -jar /home/ec2-user/code-deploy.jar > app.log 2>&1 &
